6daf64a7741e61093759a1459f85298a952a8b76
Thea33710 <thea33710@gmail.com>
2025-12-02 15:43:04 +0100

    project 0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function index_rang."""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple for pagination."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
