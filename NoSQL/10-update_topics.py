f90f4bf3a36e5a7596eab8e9d8767179ae98b44f
Thea33710 <thea33710@gmail.com>
2025-12-04 11:23:21 +0100

    end

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -10,8 +10,7 @@ def update_topics(mongo_collection, name, topics):
     """
     Updates the topics of a school document based on the name.
     """
-    result = mongo_collection.update_one(
-        {'name': name},
-        {'$set': {'topics': topics}}
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
     )
-    return result
