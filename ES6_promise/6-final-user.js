b539148ba631d9ec4a7f4eb7e51d18310a170207
Thea33710 <thea33710@gmail.com>
2025-12-18 11:12:31 +0100

    test

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const results = await Promise.allSettled([
         signUpUser(firstName, lastName),
         uploadPhoto(fileName),
-    ]).then((results) => results.map((result) => {
+    ]);
+    return results.map((result) => {
         if (result.status === 'fulfilled') {
             return { status: 'success', value: result.value };
         }
         return { status: 'error', value: String(result.reason) };
-    }));
+    });
 }
